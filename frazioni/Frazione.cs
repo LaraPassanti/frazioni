03380f6cae1b88b2330bedbde36388b4afda1103
lara.passanti@gmail.com <lara.passanti@gmail.com>
2024-01-30 17:55:51 +0100

    Aggiungere i file di progetto.

---
+++ frazioni/Frazione.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frazioni
+{
+    internal class Frazione { 
+        public int numeratore { get; set; }
+        public int denominatore { get; set; }
+        public Frazione() { }
+        public Frazione(int num, int den) { 
+            numeratore= num;
+            denominatore = den;
+        }
+        public int Mcd()
+        { int c = 1;
+            int mcd = 1;
+            while (c <= numeratore && c <= denominatore)
+            {
+                if (numeratore % c == 0 && denominatore % c == 0)
+                { mcd = c; }
+                c++;
+            }
+            return mcd; 
+        }
+        public void semplificafrazione(int macd, out int nn, out int dd)
+        {   nn=numeratore/macd;
+            dd = denominatore /macd;
+            return;
+        }
+        public override string ToString() {
+            return String.Format("{0},{1}", numeratore, denominatore);
+        }
+        public static Frazione Parse(string fraz) {
+            int a = int.Parse(fraz.Substring(0,1));
+            int b = int.Parse((fraz.Substring(2,1)));
+            Frazione p= new Frazione(a,b);
+            return p;
+
+        }
+
+    }
+}
---
+++ frazioni/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace frazioni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Frazione f = new Frazione(4,16);
+            int macd = f.Mcd();
+            int nn; int dd;
+            f.semplificafrazione(macd, out nn,out dd);  
+            Frazione semplificata=new Frazione(nn,dd);
+            Frazione pa= Frazione.Parse(semplificata.ToString());
+            Console.WriteLine(pa);
+            Console.ReadLine();
+        }
+    }
+}
